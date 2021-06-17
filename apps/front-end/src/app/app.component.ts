b67db8c55d6bda238138f0252de00dd45f956665
BenasPalivonas <palivonas.benas@gmail.com>
2021-06-17 23:40:14 +0300

    setting up nx

---
+++ apps/front-end/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'danske-task-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'front-end';
+}
