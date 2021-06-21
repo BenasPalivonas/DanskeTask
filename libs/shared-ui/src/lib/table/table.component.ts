dad27208633f4979053a7820994848be48073e49
unknown <palivonas.benas@gmail.com>
2021-06-21 17:00:55 +0300

    adding pagination to table

--- libs/shared-ui/src/lib/table/table.component.ts
+++ libs/shared-ui/src/lib/table/table.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, Input, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, Input, OnChanges, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
  export interface PeriodicElement {
@@ -12,15 +12,22 @@ import {MatTableDataSource} from '@angular/material/table';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements AfterViewInit  {
+export class TableComponent implements AfterViewInit ,OnInit  {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  //i can do this =[{name:"carPlate",displayName:"Car Plate"}]
   @Input() tableData:PeriodicElement[]=[];
-  dataSource = new MatTableDataSource<PeriodicElement>(this.tableData);
-
+  dataSource?:MatTableDataSource<PeriodicElement>;
   @ViewChild(MatPaginator) paginator?: MatPaginator;
-
+  ngOnInit(){
+  this.dataSource =new MatTableDataSource<PeriodicElement>(this.tableData);
+  }
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator||null;
+  if(this.dataSource){
+    this.dataSource.paginator = this.paginator || null;
+    }
+  }
+  changePageEvent($event:any){
+    console.log($event);
   }
 
 }
